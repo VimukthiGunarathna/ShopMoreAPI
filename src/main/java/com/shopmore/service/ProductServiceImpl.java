3dba5000b94c4903b56685e98c4115a54a0d7a78
VimukthiGunarathna <vimukthiappleid@gmail.com>
2020-11-08 21:44:30 +0530

    minor bug fix

--- src/main/java/com/shopmore/service/ProductServiceImpl.java
+++ src/main/java/com/shopmore/service/ProductServiceImpl.java
@@ -3,9 +3,11 @@ package com.shopmore.service;
 import com.shopmore.model.Product;
 import com.shopmore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
