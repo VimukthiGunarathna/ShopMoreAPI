7e4cda6d1a73a3d2893bfdc62216e84f2baa8111
VimukthiGunarathna <vimukthiappleid@gmail.com>
2020-11-09 03:51:26 +0530

    adding test cases

--- src/test/java/com/shopmore/ShopmoreApplicationTests.java
+++ src/test/java/com/shopmore/ShopmoreApplicationTests.java
@@ -1,13 +1,31 @@
 package com.shopmore;
 
+import com.shopmore.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ShopmoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Autowired
+	private ProductService service;
+
+	@Test
+	void getAllProducts() {
+		Assert.notEmpty(service.getAllProducts());
+	}
+	@Test
+	void getAllCartonPrices() {
+		Assert.notEmpty(service.getAllCartonPrices());
+	}
+	@Test
+	void getAllPricing() {
+		Assert.notEmpty(service.getAllPricing());
 	}
 
 }
